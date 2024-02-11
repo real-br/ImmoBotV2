bccfb7ca895342c2694c9116aa26046a1c696334
real-br <brecht.seuntjens@gmail.com>
2024-02-11 21:23:19 +0100

    update class archetype

--- VastgoedScraper.py
+++ VastgoedScraper.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class VastgoedScraper(ABC):
 
     @abstractmethod
-    def get_current_listings(context):
+    def get_current_listings(user_id):
         pass
 
     @abstractmethod
