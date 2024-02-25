f97186ed30de2621b97923aad64e43293a2fc909
real-br <brecht.seuntjens@gmail.com>
2024-02-25 23:35:59 +0100

    use jobqueu of telegram

--- bot.py
+++ bot.py
@@ -17,9 +17,7 @@ import config
 import glob
 
 from telegram import Update
-from telegram.ext import (
-    Application,
-)
+from telegram.ext import Application, CallbackContext, JobQueue
 from telegram.error import BadRequest
 from sqlite import get_listing_info_for_message, get_user_ids, get_username
 
@@ -48,16 +46,22 @@ async def main():
     """Start the bot."""
     application = Application.builder().token(TOKEN).build()
 
-    user_ids = get_user_ids("databases/user_data.sqlite", "user_data")
-
     conv_handler = conversation_handler()
 
     application.add_handler(conv_handler)
 
-    asyncio.create_task(update_checker_logic(application, user_ids))
-    logger.info(f"update_checker scheduled to run every {config.UPDATE_PERIOD} seconds")
+    # Create JobQueue instance
+    job_queue = JobQueue(application)
 
-    await application.run_polling(allowed_updates=Update.ALL_TYPES)
+    # Add the user_ids to the context and schedule the update_checker_logic job
+    job_queue.run_repeating(
+        update_checker_logic,
+        interval=config.UPDATE_PERIOD,
+        first=0,
+    )
+
+    logger.info(f"update_checker scheduled to run every {config.UPDATE_PERIOD} seconds")
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 def generate_saved_listing_response_from_db(db_name, table_name, immo_name, listing):
@@ -71,55 +75,55 @@ def generate_saved_listing_response_from_db(db_name, table_name, immo_name, list
     return caption, img_url
 
 
-async def update_checker_logic(application: Application, user_ids: list):
-    while True:
-        scraper: VastgoedScraper
-        for scraper in scrapers:
-            logger.info(f"Checking for new listings from {scraper.get_scraper_name()}")
-            current_listings_immoweb = {}
-            if scraper.get_scraper_name() == "Immoweb":
-                current_listings_immoweb = scraper.get_current_listings()
+async def update_checker_logic(context: CallbackContext):
+    application: Application = context.bot
+    user_ids = get_user_ids("databases/user_data.sqlite", "user_data")
 
-            for user_id in user_ids:
-                immmo_name = scraper.get_scraper_name()
-                db_name = scraper.get_db_name()
-                listing_table_name = scraper.get_listing_table_name()
-                current_listings = (
-                    scraper.get_current_listings(user_id)
-                    if scraper.get_scraper_name() != "Immoweb"
-                    else current_listings_immoweb
-                )
+    for scraper in scrapers:
+        logger.info(f"Checking for new listings from {scraper.get_scraper_name()}")
+        current_listings_immoweb = {}
+        if scraper.get_scraper_name() == "Immoweb":
+            current_listings_immoweb = scraper.get_current_listings()
 
-                new_listings = scraper.store_and_return_new_listings(
-                    current_listings, user_id
-                )
-                try:
-                    logger.info(f"Found {len(new_listings)} new listings")
-                    for new_listing in new_listings:
-                        listing_caption, listing_photo_url = (
-                            generate_saved_listing_response_from_db(
-                                db_name, listing_table_name, immmo_name, new_listing
-                            )
+        for user_id in user_ids:
+            immmo_name = scraper.get_scraper_name()
+            db_name = scraper.get_db_name()
+            listing_table_name = scraper.get_listing_table_name()
+            current_listings = (
+                scraper.get_current_listings(user_id)
+                if scraper.get_scraper_name() != "Immoweb"
+                else current_listings_immoweb
+            )
+
+            new_listings = scraper.store_and_return_new_listings(
+                current_listings, user_id
+            )
+            try:
+                logger.info(f"Found {len(new_listings)} new listings")
+                for new_listing in new_listings:
+                    listing_caption, listing_photo_url = (
+                        generate_saved_listing_response_from_db(
+                            db_name, listing_table_name, immmo_name, new_listing
                         )
-                        try:
-                            await send_listing_photo(
-                                application, user_id, listing_photo_url, listing_caption
-                            )
-                        except Exception as e:
-                            logger.error(
-                                f"Failed to send listing photo and caption. Error: {str(e)}"
-                            )
-                            logger.exception(e)
-                except Exception as e:
-                    logger.error(f"Failed to process new listings. Error: {str(e)}")
-                    logger.exception(e)
+                    )
+                    try:
+                        await send_listing_photo(
+                            application, user_id, listing_photo_url, listing_caption
+                        )
+                    except Exception as e:
+                        logger.error(
+                            f"Failed to send listing photo and caption. Error: {str(e)}"
+                        )
+                        logger.exception(e)
+            except Exception as e:
+                logger.error(f"Failed to process new listings. Error: {str(e)}")
+                logger.exception(e)
 
-        current_time = datetime.now()
-        next_run_time = current_time + timedelta(seconds=config.UPDATE_PERIOD)
-        logger.info(
-            f"Update checker executed at {current_time}, next run time for update_checker: {next_run_time}"
-        )
-        await asyncio.sleep(config.UPDATE_PERIOD)
+    current_time = datetime.now()
+    next_run_time = current_time + timedelta(seconds=config.UPDATE_PERIOD)
+    logger.info(
+        f"Update checker executed at {current_time}, next run time for update_checker: {next_run_time}"
+    )
 
 
 async def send_listing_photo(application, user_id, listing_photo_url, listing_caption):
